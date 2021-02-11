3e9b9b3051656ebfefd192ba3c561e9e1f3535e3
Angel Baro <angelbaro44@gmail.com>
2021-02-11 00:47:14 -0500

    Added Teast Cases

--- index.js
+++ index.js
@@ -6,14 +6,24 @@
 function likes(names) {
       switch(names.length) {
         case 0:
-          return 'no one likes this';
+          return '0 likes';
         case 1:
-          return `${names[0]} likes this`;
+          return `${names[0]} likes this!`;
         case 2:
-          return `${names[0]} and ${names[1]} like this`;
+          return `${names[0]} and ${names[1]} like this!`;
         case 3:
-          return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+          return `${names[0]}, ${names[1]} and ${names[2]} like this!`;
         default:
-          return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+          return `${names[0]}, ${names[1]} and ${names.length - 2} others like this!`;
       }
     }
+
+
+
+// Test Cases
+
+console.log(likes([]));   //Expected Results: '0 likes');
+console.log(likes(['Peter']));   //Expected Results: 'Peter likes this!');
+console.log(likes(['Jacob', 'Alex']));   //Expected Results: 'Jacob and Alex like this!');
+console.log(likes(['Max', 'John', 'Mark']));   //Expected Results: 'Max, John and Mark like this!');
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));   //Expected Results: 'Alex, Jacob and 2 others like this!');
